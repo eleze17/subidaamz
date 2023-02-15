1d4d35ae21f5d768c6d085d9e463c63c1b4be202
eleze17 <ezeenriquez17@gmail.com>
2023-02-15 13:35:08 -0300

    commit

--- pages/api/upload-url.ts
+++ pages/api/upload-url.ts
@@ -1,98 +1,31 @@
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
-import { NextApiRequest, NextApiResponse } from 'next'
 import { createPresignedPost } from '@aws-sdk/s3-presigned-post';
-import dotenv from 'dotenv'
-import nextConnect from "next-connect";
-import { createReadStream, unlink } from 'fs';
-import multer from 'multer'
+import { NextApiRequest, NextApiResponse } from 'next';
 
-dotenv.config()
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const s3Client = new S3Client({
+    region: process.env.REGION,
+    credentials: {
+      accessKeyId: process.env.ACCESS_KEY || '',
+      secretAccessKey: process.env.SECRET_KEY || '' ,
+    },
+  });
 
-const s3Client = new S3Client({
-  region: process.env.REGION,
-  credentials: {
-    accessKeyId: process.env.ACCESS_KEY || '',
-    secretAccessKey: process.env.SECRET_KEY || '',
-  },
-});
+  const post = await createPresignedPost(s3Client, {
+    Bucket: process.env.S3_BUCKET_NAME || '',
+    Key: req.query.file?.toString() || '' ,
+    Fields: {
+      acl: 'public-read',
+      'Content-Type': req.query.fileType?.toString() || '',
+    },
+    Expires: 600, // seconds
+    Conditions: [
+      ['content-length-range', 1048576, 10485760], // up to 1 MB
+    ],
+  });
 
-interface reqFile extends NextApiRequest{
-  file:{
-    filename:string,
-    path:string
-  },
-
-
+  res.status(200).json(post);
 }
-
-const uploadToBucket =  async (req: reqFile) => {
-    const stream = createReadStream(req.file.path);
-     const params = {
-         Bucket:process.env.S3_BUCKET_NAME!,
-         Key:req.file.filename?.toString() || '',
-         Body:stream
-     };
-
-       try {
-         const data = await s3Client.send(new PutObjectCommand(params));
-         return data; // For unit tests.
-       } catch (err) {
-         console.log("Error", err);
-       }
- };
-
-const storage = multer.diskStorage({
-  destination: function (req: Express.Request, file: Express.Multer.File, callback: (error: Error | null, destination: string) => void) {
-   callback(null, "data");
-  null
-},
-filename: function (req: Express.Request, file: Express.Multer.File, callback: (error: Error | null, filename: string) => void) {
-  
-  callback(null,file.fieldname + '-' + Date.now() + '.jpg')
-  null
-
-}
-});
-const  imagenes = multer({storage:storage})
-
-const apiRoute = nextConnect({
-  //Handle any other HTTP method
-  onNoMatch(req: NextApiRequest, res:NextApiResponse) {
-    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
-  },
-});
-
-const uploadMiddleware =  imagenes.single('file')
-
-apiRoute.use(uploadMiddleware)
-   
-export default apiRoute.post( (req:reqFile, res:NextApiResponse)=> {
-
-  console.log(req.file)
-    uploadToBucket(req)
-    
-    .then( () =>{unlink(req.file.path, (err) => {
-      if (err) throw err;
-      console.log('path/file.txt was deleted');
-    });
-
-res.send('cargado')
-    
-   })})
-  
-     
-  export const config = {
-      api: {
-        bodyParser:false,
-          //sizeLimit: '5mb',
-        },
-    } 
-
-
-
-  
-
- 
-
-
-  
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,45 @@
 export default function Upload() {
   return (
     <>
-   <form action="/api/upload-url" method="POST" encType="multipart/form-data">
-      <input type="file" name="file" />
-      <input type="submit" className="btn btn-info" value="Enviar" ></input>
-    </form>
-
+     <>
+      <p>Upload a .png or .jpg image (max 10MB).</p>
+      <input
+        onChange={uploadPhoto}
+        type="file"
+        accept="image/png, image/jpeg"
+      />
+    </>
    
     </>
   )
+}
+
+const uploadPhoto = async (e: React.ChangeEvent<HTMLInputElement>) => {
+  const file = e.target.files?.[0]!
+  console.log(file)
+  const filename = encodeURIComponent(file.name)
+  const fileType = encodeURIComponent(file.type)
+
+  const res = await fetch(
+    `/api/upload-url?file=${filename}&fileType=${fileType}`
+  )
+  const { url, fields } = await res.json()
+  const formData = new FormData()
+
+  Object.entries({ ...fields, file }).forEach(([key, value]) => {
+    formData.append(key, value as string)
+    
+  })
+
+  
+  fetch(url, {
+    method: 'POST',
+    body: formData,
+    mode: 'no-cors'
+   }
+  )
+.then(res => alert('archivo cargado')
+)
 }
 
 
